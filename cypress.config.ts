c0091c4ba3f4a991532ad8d4d7fb5c0f4db19851
olawale <olapetanwale@outlook.com>
2024-06-09 22:45:14 +0100

    Add mochawesome for reporting

---
+++ cypress.config.ts
@@ -0,0 +1,28 @@
+const createBuilder = require('@bahmutov/cypress-esbuild-preprocessor')
+const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin
+const createEsBuilderPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {    
+    async setupNodeEvents(on: any, config: any) {
+      // implement node event listeners here
+      const builder = createBuilder({
+        plugins: [createEsBuilderPlugin(config)],
+      });
+      on('file:preprocessor', builder);
+      await addCucumberPreprocessorPlugin(on, config)
+      return config;
+    },
+    specPattern: 'cypress/e2e/features/*.feature',
+    env: {
+      baseUrl: 'https://www.dummyticket.com'
+    }
+  },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    mochaFile: 'result/testReport.html',
+    toConsole: true,
+  },
+  watchForFileChanges: false
+});
